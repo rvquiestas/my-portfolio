1e7458e1373d103e3a68874dbddb5702b3dd620b
rvquiestas <rvquiestas@gmail.com>
2025-11-03 17:33:11 +0800

    Update title size in Title.jsx

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Title = ({text1,text2}) => {
   return (
     <div className='inline-flex gap-2 items-center mb-3 uppercase'>
-      <p className='text-primary text-2xl 2xl:text-4xl font-bold underline decoration-secondary underline-offset-8'>{text1} <span className='text-primary font-bold underline decoration-secondary'>{text2}</span></p>
+      <p className='text-primary text-4xl xl:text-5xl font-bold underline decoration-secondary underline-offset-8'>{text1} <span className='text-primary font-bold underline decoration-secondary'>{text2}</span></p>
     </div>
   )
 }
