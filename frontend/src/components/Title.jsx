fa1f2bdcf63b5b2814fa4400ff1bacaddcb6d084
rvquiestas <rvquiestas@gmail.com>
2025-11-03 21:51:40 +0800

    Fix title responsiveness

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -2,7 +2,7 @@
 const Title = ({text1,}) => {
   return (
     <div className='inline-flex gap-2 items-center mb-3 uppercase'>
-      <p className='text-primary text-2xl xl:text-4xl font-bold underline decoration-secondary underline-offset-8'>{text1}</p>
+      <p className='text-primary text-2xl lg:text-4xl font-bold underline decoration-secondary underline-offset-8'>{text1}</p>
     </div>
   )
 }
