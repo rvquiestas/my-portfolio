5efee68b974d98dafa76aa9491e4999c3e611a1d
rvquiestas <rvquiestas@gmail.com>
2025-11-06 04:40:11 +0800

    Add 'View UI Design' in my Portfolio Website

--- frontend/src/components/ProjectsCard.jsx
+++ frontend/src/components/ProjectsCard.jsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '../Context/ThemeContext';
 import { GrGithub } from "react-icons/gr";
 import { IoEye, IoEyeOff } from "react-icons/io5";
+import { FiFigma } from "react-icons/fi";
 import { assets } from "../assets/assets.js";
 
 const ProjectsCard = () => {
@@ -16,8 +17,8 @@ const ProjectsCard = () => {
             src={theme === "dark" ? assets.darkProj1 : assets.lightProj1}
             />
             <div className='flex items-center justify-evenly rounded-b-lg pt-4 pb-8 bg-project-bg text-primary'>
-                <a href="https://github.com/rvquiestas/MGWRPC-WEBSITE" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><GrGithub size={20} className="sm:w-6 sm:h-6"/>View on GitHub</a>
-                <a href="https://mgwrpc-frontend.vercel.app/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><IoEye size={20} className="sm:w-6 sm:h-6"/>Live Preview</a>
+                <a href="https://github.com/rvquiestas/MGWRPC-WEBSITE" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><GrGithub className="sm:w-6 sm:h-6"/>View on GitHub</a>
+                <a href="https://mgwrpc-frontend.vercel.app/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><IoEye className="sm:w-6 sm:h-6"/>Live Preview</a>
             </div>
         </div>
 
@@ -29,8 +30,8 @@ const ProjectsCard = () => {
             src={theme === "dark" ? assets.darkProj1a : assets.lightProj1a}
             />
             <div className='flex items-center justify-evenly rounded-b-lg pt-4 pb-8 bg-project-bg text-primary'>
-                <a href="https://github.com/rvquiestas/MGWRPC-WEBSITE" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><GrGithub size={20} className="sm:w-6 sm:h-6"/>View on GitHub</a>
-                <a href="https://mgwrpc-website.vercel.app/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><IoEye size={20} className="sm:w-6 sm:h-6"/>Live Preview</a>
+                <a href="https://github.com/rvquiestas/MGWRPC-WEBSITE" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><GrGithub className="sm:w-6 sm:h-6"/>View on GitHub</a>
+                <a href="https://mgwrpc-website.vercel.app/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><IoEye className="sm:w-6 sm:h-6"/>Live Preview</a>
             </div>
         </div>
 
@@ -42,8 +43,8 @@ const ProjectsCard = () => {
             src={theme === "dark" ? assets.darkProj2 : assets.lightProj2}
             />
             <div className='flex items-center justify-evenly rounded-b-lg pt-4 pb-8 bg-project-bg text-primary'>
-                <a href="https://github.com/rvquiestas/SKUDL_Website" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><GrGithub size={20} className="sm:w-6 sm:h-6"/>View on GitHub</a>
-                <a href="" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 pointer-events-none"><IoEyeOff size={20} className="sm:w-6 sm:h-6"/>No Preview</a>
+                <a href="https://github.com/rvquiestas/SKUDL_Website" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><GrGithub className="sm:w-6 sm:h-6"/>View on GitHub</a>
+                <a href="" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 pointer-events-none"><IoEyeOff className="sm:w-6 sm:h-6"/>No Preview</a>
             </div>
         </div>
         
@@ -55,8 +56,8 @@ const ProjectsCard = () => {
             src={theme === "dark" ? assets.darkProj3 : assets.lightProj3}
             />
             <div className='flex items-center justify-evenly rounded-b-lg pt-4 pb-8 bg-project-bg text-primary'>
-                <a href="https://github.com/rvquiestas/my-portfolio" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><GrGithub size={20} className="sm:w-6 sm:h-6"/>View on GitHub</a>
-                <a href="" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 pointer-events-none"><IoEyeOff size={20} className="sm:w-6 sm:h-6"/>No Preview</a>
+                <a href="https://github.com/rvquiestas/my-portfolio" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><GrGithub className="sm:w-6 sm:h-6"/>View on GitHub</a>
+                <a href="https://www.figma.com/design/je3wT5hlCSuQoxOZ0hTFGU/MY-PORTFOLIO-WEBSITE?m=auto&t=9w72PmC9oSWHOfaZ-1" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-secondary"><FiFigma className="sm:w-6 sm:h-6"/>View UI Design</a>
             </div>
         </div>
     </div>
